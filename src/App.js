2bfea54e1793054a4d3964870f5c45cb812a048c
Manisha-95 <raosmanisha@gmail.com>
2020-09-19 16:27:10 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component{
                 <div className="row">
                     <div className="col-md-12">
                         <Router>
-                            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                            <Navbar bg="light" variant="light" expand="lg" sticky="top">
                                 <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
@@ -30,13 +30,15 @@ class App extends React.Component{
                                     <Nav.Link href="/">Home</Nav.Link>
                                     <Nav.Link href="/Shop">Shop</Nav.Link>
                                     <Nav.Link href="/Magazine">Magazine</Nav.Link>
-                                   <ShoppingCartOutlinedIcon style={{fontSize:"20px"}}/>
+                                  
                                     </Nav>
                                 <form class="form-inline">
       <div class="md-form my-0">
         <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
       </div>
     </form>
+     <ShoppingCartOutlinedIcon style={{fontSize:"20px"}}/>
+    <div>LOGIN</div>
                                 </Navbar.Collapse>
                             </Navbar>
                             <br />
